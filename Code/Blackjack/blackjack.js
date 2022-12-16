94f87e689e8703a69ba242986ab7a21d118e4c04
Peter Riesing <peter.riesing@gmail.com>
2022-12-16 09:18:37 -0500

    blackjack

--- Code/Blackjack/blackjack.js
+++ Code/Blackjack/blackjack.js
@@ -21,8 +21,8 @@ const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"]
 // character sums
 let dealerSum = 0
 let playerSum = 0
-let canHit = true
-let canDeal = true
+let playerCards = []
+let dealerCards = []
 
 // makeDeck function(rank & suit arguements)
 const makeDeck = (rank, suit) => {
@@ -53,69 +53,97 @@ for (let suit of suits) {
     }
 }
 
-// =========== SHUFFLE FUNCTION =========== 
-// geeks for geeks solution 1 - breaks the file
-// const shuffle = (array) => {
-//     for (var i = array.length -1; i > 0; i++) {
-//         var j = Math.floor(Math.random() * (i + 1));
-//         var temp = array[i];
-//         array[i] = array[j];
-//         array[j] = temp;
-//     }
-//     return array
-// }
-// =========== SHUFFLE FUNCTION 2 ===========
+// =========== CHECK ACE FUNCTION ===========
+const checkAce = (points,arr) =>{
+    if (points > 21){
+        arr.forEach((element)=>{
+        if(element.rank === 'ace'){
+            element.pointValue = 1
+        }
+    })
+    }
+}
+
+// =========== SHUFFLE FUNCTION ===========
 // geeks for geeks solution 2 - this works
 const shuffle = (array) => {
     return array.sort(()=>Math.random()-0.5)
 }
+const getCard = () =>{
+    return deck.pop();
+}
+const getImage = (card) => {
+    cardImage = document.createElement('img')
+    cardImage.src = `./images/${card.rank}_of_${card.suit}.png` 
+    return cardImage
+}
+const playerRender = (arr) =>{
+    arr.forEach((element) => {
+        playerHand.append(getImage(element))
+    });
+}
+const dealerRender = (arr) =>{
+    arr.forEach((element) => {
+        dealerHand.append(getImage(element))
+    });
+}
+
+const playerPointCalc = (arr) => {
+    let points = 0
+    for(const i of arr){
+        points+= i.pointValue
+    }
+    playerSum = points
+    return playerSum
+}
+const dealerPointCalc = (arr) => {
+    let points = 0
+    for(const i of arr){
+        points+= i.pointValue
+    }
+    dealerSum = points
+    return dealerSum
+}
 
 // =========== DEAL FUNCTION ===========
 const deal = () =>{
-    if(canDeal != true){
-        return
-    }
-    while(dealerSum < 17) {
-        let card = deck.pop()
-        let cardImg = document.createElement("img")
-        cardImg.src = card.src
-        dealerSum += card.pointValue
-        document.getElementById("dealer-hand").append(cardImg)
-    }
-    for (let i = 0; i < 2; i++) {
-        let card = deck.pop()
-        let cardImg = document.createElement("img")
-        cardImg.src = card.src
-        playerSum += card.pointValue
-        document.getElementById("player-hand").append(cardImg)
-        const playerPoints = document.getElementById("player-points")
-        playerPoints.innerText = playerSum
-    } console.log(deck)
+    dealButton.disabled = true
+    dealerCards.push(getCard())
+    dealerCards.push(getCard())
+    dealerRender(dealerCards)
+    checkAce(dealerPointCalc(dealerCards), dealerCards)
+
+    playerCards.push(getCard())
+    playerCards.push(getCard())
+    playerRender(playerCards)
+    playerValue.innerText = playerPointCalc(playerCards)
+    checkAce(playerPointCalc(playerCards), playerCards)
 }
 
 // =========== HIT FUNCTION ===========
 const hit = () =>{
-    canDeal = false
-    if(canHit != true){
-        return
-    }for (let i = 0; i < 1; i++) {
-        let card = deck.pop()
-        let cardImg = document.createElement("img")
-        cardImg.src = card.src
-        playerSum += card.pointValue
-        document.getElementById("player-hand").append(cardImg)
-        const playerScore = document.getElementById("player-points")
-        playerScore.innerText = playerSum
-    }
+    playerHand.innerHTML = null
+    playerCards.push(getCard())
+    playerRender(playerCards)
+    checkAce(playerPointCalc(playerCards), playerCards)
+    playerValue.innerText = playerPointCalc(playerCards)
     if(playerSum >= 21){
-        canHit = false
+        hitButton.disabled = true
+        // kills the ability to stand because player busted
+        stand()
     }
-    console.log(deck)
 }
 // =========== STAND FUNCTION ===========
 const stand = () =>{
-    canDeal = false
-    canHit = false
+    dealButton.disabled = true
+    hitButton.disabled = true
+    while (dealerSum < 17) {
+        dealerHand.innerHTML = null
+        dealerCards.push(getCard())
+        dealerRender(dealerCards)
+        checkAce(dealerPointCalc(dealerCards), dealerCards)
+        dealerValue.innerText = dealerPointCalc(dealerCards)
+    }
     let message = ""
     // player sum is more than dealer sum && player sum is less than or equal to 21
     if(playerSum > dealerSum && playerSum <= 21){
@@ -133,16 +161,17 @@ const stand = () =>{
     }else if(playerSum === dealerSum){
         message = "It's A Draw"
     }
-    const dealerScore = document.getElementById("dealer-points")
-    dealerScore.innerText = dealerSum
     const messageText = document.getElementById("message")
     messageText.innerText = message
+    const dealerScore = document.getElementById("dealer-points")
+    dealerScore.innerText = dealerSum
     return
 }
 
 
 // load game when window opened
 window.addEventListener("DOMContentLoaded", () =>{
+
     shuffle(deck)
     console.log(deck)
     dealButton.addEventListener("click", deal)
